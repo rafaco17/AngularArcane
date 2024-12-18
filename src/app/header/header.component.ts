03d1a8da7540cdc93883f2d6e5f951b44cb9e331
rafaco17 <riechevarriam@unjbg.edu.pe>
2024-12-18 11:42:04 -0500

    Added logic in scroll for header

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit, Renderer2 } from '@angular/core';
 import { ItemHeaderComponent } from '../item-header/item-header.component';
 
 @Component({
@@ -7,9 +7,30 @@ import { ItemHeaderComponent } from '../item-header/item-header.component';
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit, OnDestroy {
   items = [
     { description : 'noticias', href : 'https://www.youtube.com/watch?v=qaTfRV5XwNc&list=RDqaTfRV5XwNc&start_radio=1' },
     { description : 'creador de avatares', href : '/' }
   ]
+
+  private scrollListener!: () => void
+
+  constructor(private renderer: Renderer2) {}
+
+  ngOnInit(): void {
+    this.scrollListener = this.renderer.listen('window','scroll',() => {
+      const header = document.getElementById('header')
+      if(window.scrollY > 82) {
+        header?.classList.add('scrolled')
+      } else {
+        header?.classList.remove('scrolled')
+      }
+    })
+  }
+
+  ngOnDestroy(): void {
+    if(this.scrollListener) {
+      this.scrollListener()
+    }
+  }
 }
