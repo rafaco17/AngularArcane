50dcb932c4c81e502cbcb0c0194dac58b39a6cc6
rafaco17 <riechevarriam@unjbg.edu.pe>
2024-12-19 23:23:47 -0500

    Improving logic in carousel

--- src/app/small-carousel/small-carousel.component.ts
+++ src/app/small-carousel/small-carousel.component.ts
@@ -17,35 +17,27 @@ export class SmallCarouselComponent {
     { name : 'Avance de Arcane en los juegos: Come Play', image : 'https://cmsassets.rgpub.io/sanity/images/dsfx7636/news/9ff86200eed0c2566469e2f5834890df2b0c9050-1920x1080.jpg?auto=format&fit=fill&q=80&w=413',  href : 'https://www.youtube.com/watch?v=rR5vyzjGwmk&embeds_referring_euri=https%3A%2F%2Fwww.arcane.com%2F' }
   ]
 
-  private isDragging = false; // Estado de arrastre
-  private startX = 0;         // Posición inicial del puntero
-  private scrollLeft = 0;     // Posición inicial del scroll
-  
-  // Comienza el arrastre
-  onDragStart(event: MouseEvent): void {
-    const carousel = event.currentTarget as HTMLElement;
-    this.isDragging = true;
-    this.startX = event.pageX - carousel.offsetLeft;
-    this.scrollLeft = carousel.scrollLeft;
-    carousel.style.cursor = 'grabbing'; // Cambia el cursor durante el arrastre
-  }
+  private currentTranslateX: number = 0
+  private snapPoints: number[] = [0, -354, -670]
 
-  // Durante el arrastre
-  onDragMove(event: MouseEvent): void {
-    if (!this.isDragging) return;
-
-    const carousel = event.currentTarget as HTMLElement;
-    const x = event.pageX - carousel.offsetLeft;
-    const walk = x - this.startX; // Distancia arrastrada
-    carousel.scrollLeft = this.scrollLeft - walk;
-  }
-
-  // Termina el arrastre
-  onDragEnd(): void {
-    this.isDragging = false;
-    const carousel = document.querySelector('.carousel_container') as HTMLElement;
-    if (carousel) {
-      carousel.style.cursor = 'grab'; // Restaura el cursor original
+  onForward(): void {
+    const currentIndex = this.snapPoints.indexOf(this.currentTranslateX)
+    if(currentIndex < this.snapPoints.length - 1) {
+      this.currentTranslateX = this.snapPoints[currentIndex + 1]
+      this.updateTransform(this.currentTranslateX)
     }
+  } 
+
+  onBackward(): void {
+    const currentIndex = this.snapPoints.indexOf(this.currentTranslateX)
+    if(currentIndex !== 0) {
+      this.currentTranslateX = this.snapPoints[currentIndex - 1]
+      this.updateTransform(this.currentTranslateX)
+    }
+  }
+
+  private updateTransform(value: number): void {
+    const itemsContainer = document.querySelector('.container_items') as HTMLElement;
+    itemsContainer.style.transform = `translate3d(${value}px, 0, 0)`;
   }
 }
